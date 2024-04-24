d8368c183d32f24b0c7463f6604db63d2755800d
Miko <89363492+MikoGome@users.noreply.github.com>
2024-04-24 01:52:40 -0700

    more cors

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ const mangaSearch = require("./routes/mangaSearch")
 app.use(bodyParser.json())
 app.use((req, res, next) => {
   res.setHeader('Access-Allow-Control-Origin', '*');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   return next();
 });
 require('dotenv').config()
