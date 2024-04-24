70bbc9ae2d840c9a1fc05d78cabdc74c94d20a9a
Miko <89363492+MikoGome@users.noreply.github.com>
2024-04-24 14:18:31 -0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -4,25 +4,11 @@ const bodyParser = require("body-parser")
 const mangaRouter = require("./routes/mangaRouter")
 const mangaListRouter = require("./routes/mangaListRouter")
 const mangaSearch = require("./routes/mangaSearch")
-const cors = require('cors');
 
 app.use(bodyParser.json())
-app.use((req, res, next) => {
+app.option((req, res, next) => {
   // Allow any domain to access your server
   res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Allow specific HTTP methods
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Allow headers like Authorization, Content-Type, etc.
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-
-  // Handle pre-flight requests for CORS
-  if (req.method === 'OPTIONS') {
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    return res.status(200).end();
-  }
-
   next();
 })
 
